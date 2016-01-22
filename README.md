dd37600fabda32dd62b0203df945c03043356a26
tim <llStrainll@gmail.com>
2016-01-23 00:12:00 +0300

    add optional output 2 file or stdout, update readme

--- README.md
+++ README.md
@@ -3,17 +3,24 @@
 Demo for [randomex](https://github.com/timCF/randomex) generator. This console application gives output to stdout and to files. Indexed files are in ./output dir. Some flags to generate specific output :
 
 ```
---ranges --samples --sets : generates stdout and text file with given params
+--ranges --samples --sets : generates output (text file or stdout) with given params
 
---diehard : generates binary file for diehard tests with given number of integers (diehard > 0)
---decks --cards : generates stdout and text file for card games (decks > 0 , cards = 24 | 32 | 36 | 52 | 54)
---ball-min --ball-max --ball-num --spin-num : generates stdout and text file for bingo/keno-like games
+--diehard : generates output (binary file or stdout) for diehard tests with given number of integers (diehard > 0)
+--decks --cards : generates output (text file or stdout) for card games (decks > 0 , cards = 24 | 32 | 36 | 52 | 54)
+--ball-min --ball-max --ball-num --spin-num : generates output (text file or stdout) for bingo/keno-like games
+```
+
+Some mandatory output options :
+
+```
+--write-to-file : true | false
+--stdout : true | false
 ```
 
 Example for dieharder tests
 
 ```
-mix run -e "" --no-halt console --diehard 100000000
+mix run -e "" --no-halt console --diehard 100000000 --write-to-file true --stdout false
 dieharder -f ./output/1452697128384077.txt -g 201 -a
 ```
 
@@ -27,17 +34,17 @@ This way you can run it without elixir / erlang in your system. There are builds
 ```
 ~ $ wget https://github.com/timCF/randomex_demo/releases/download/0.0.1/linux64bit.tar.gz
 ~ $ tar zxf ./linux64bit.tar.gz
-~ $ ./bin/randomex_demo console --ranges 32,111 --samples 500,1000,1500 --sets 1
-~ $ ./bin/randomex_demo console --diehard 10000000
-~ $ ./bin/randomex_demo console --decks 1000 --cards 36
-~ $ ./bin/randomex_demo console --ball-min 1 --ball-max 80 --ball-num 20 --spin-num 1000
+~ $ ./bin/randomex_demo console --ranges 32,111 --samples 500,1000,1500 --sets 1 --write-to-file true --stdout false
+~ $ ./bin/randomex_demo console --diehard 10000000 --write-to-file true --stdout false
+~ $ ./bin/randomex_demo console --decks 1000 --cards 36 --write-to-file true --stdout false
+~ $ ./bin/randomex_demo console --ball-min 1 --ball-max 80 --ball-num 20 --spin-num 1000 --write-to-file true --stdout false
 ~ $ ls ./output/
 1452701335999535.txt  1452701387010760.txt  1452701420327234.txt  1452701450936883.txt  index.txt
 ~ $ cat ./output/index.txt
-1452701335999535 : {"ranges":[32,111],"samples":[500,1000,1500],"sets":1}
-1452701387010760 : {"diehard":10000000}
-1452701420327234 : {"cards":36,"decks":1000}
-1452701450936883 : {"ball_max":80,"ball_min":1,"ball_num":20,"spin_num":1000}
+1452701335999535 : {"ranges":[32,111],"samples":[500,1000,1500],"sets":1,"stdout":false,"write_to_file":true}
+1452701387010760 : {"diehard":10000000,"stdout":false,"write_to_file":true}
+1452701420327234 : {"cards":36,"decks":1000,"stdout":false,"write_to_file":true}
+1452701450936883 : {"ball_max":80,"ball_min":1,"ball_num":20,"spin_num":1000,"stdout":false,"write_to_file":true}
 ```
 
 Compile
@@ -51,8 +58,8 @@ tim@tim-VirtualBox:~/tmp$ cd randomex_demo/
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix local.hex --force
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix local.rebar --force
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix deps.get && mix compile
-tim@tim-VirtualBox:~/tmp/randomex_demo$ mix run -e "" --no-halt console --ranges 32,111 --samples 500,1000,1500 --sets 1
-tim@tim-VirtualBox:~/tmp/randomex_demo$ mix run -e "" --no-halt console --diehard 10000000
+tim@tim-VirtualBox:~/tmp/randomex_demo$ mix run -e "" --no-halt console --ranges 32,111 --samples 500,1000,1500 --sets 1 --write-to-file true --stdout false
+tim@tim-VirtualBox:~/tmp/randomex_demo$ mix run -e "" --no-halt console --diehard 10000000 --write-to-file true --stdout false
 tim@tim-VirtualBox:~/tmp/randomex_demo$ cd ./output/
 tim@tim-VirtualBox:~/tmp/randomex_demo/output$ ls
 1446760892646954.txt  1446760892786954.txt  index.txt
@@ -70,8 +77,8 @@ tim@tim-VirtualBox:~/tmp/randomex_demo$ mix local.hex --force
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix local.rebar --force
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix deps.get && mix compile
 tim@tim-VirtualBox:~/tmp/randomex_demo$ mix release
-tim@tim-VirtualBox:~/tmp/randomex_demo$ rel/randomex_demo/bin/randomex_demo console --ranges 32,111 --samples 500,1000,1500 --sets 1
-tim@tim-VirtualBox:~/tmp/randomex_demo$ rel/randomex_demo/bin/randomex_demo console --diehard 10000000
+tim@tim-VirtualBox:~/tmp/randomex_demo$ rel/randomex_demo/bin/randomex_demo console --ranges 32,111 --samples 500,1000,1500 --sets 1 --write-to-file true --stdout false
+tim@tim-VirtualBox:~/tmp/randomex_demo$ rel/randomex_demo/bin/randomex_demo console --diehard 10000000 --write-to-file true --stdout false
 tim@tim-VirtualBox:~/tmp/randomex_demo$ ls ./rel/randomex_demo/output/
 1446760892646954.txt  1446760892786954.txt  index.txt
 ```
--- lib/main.ex
+++ lib/main.ex
@@ -15,11 +15,11 @@ defmodule RandomexDemo.Main do
 			{new_args, [], []} ->
 				case Enum.reduce_while(new_args, %{}, &parse_proc/2)  do
 					{:error, bin} when is_binary(bin) -> IO.puts(bin)
-					resmap = %{diehard: n} when ((n > 0) and is_integer(n)) -> generate(resmap, &generate_diehard/2)
-					resmap = %{ranges: _, samples: _, sets: _} -> generate(resmap, &generate_main/2)
-					resmap = %{decks: _, cards: cards} when (cards in [24, 32, 36, 52, 54]) -> generate(resmap, &generate_cards/2)
-					resmap = %{ball_min: min, ball_max: max, ball_num: num, spin_num: _} when (min <= max) and (num <= (max - min + 1)) -> generate(resmap, &generate_balls/2)
-					some -> IO.puts("ERROR , wrong arguments #{inspect some}")
+					resmap = %{diehard: n, write_to_file: _, stdout: _} when ((n > 0) and is_integer(n)) -> generate(resmap, &generate_diehard/2)
+					resmap = %{ranges: _, samples: _, sets: _, write_to_file: _, stdout: _} -> generate(resmap, &generate_main/2)
+					resmap = %{decks: _, cards: cards, write_to_file: _, stdout: _} when (cards in [24, 32, 36, 52, 54]) -> generate(resmap, &generate_cards/2)
+					resmap = %{ball_min: min, ball_max: max, ball_num: num, spin_num: _, write_to_file: _, stdout: _} when (min <= max) and (num <= (max - min + 1)) -> generate(resmap, &generate_balls/2)
+					some -> IO.puts("ERROR , wrong or some missing arguments, got #{inspect some}")
 				end
 			{_,some,errors} ->
 				IO.puts("ERROR , got unparsed #{inspect some} and errors #{inspect errors}")
@@ -38,6 +38,7 @@ defmodule RandomexDemo.Main do
 			_ -> {:halt, {:error, "ERROR , sets must be int > 0 , incorrect arg #{bin}"}}
 		end
 	end
+	defp parse_proc({key,val}, acc) when (key in [:write_to_file, :stdout]) and (val in ["true", "false"]), do: {:cont, Map.put(acc, key, String.to_atom(val))}
 	defp parse_proc({k,v}, _), do: {:halt, {:error, "ERROR , unknown argument #{k} #{v}"}}
 
 	#
@@ -48,7 +49,7 @@ defmodule RandomexDemo.Main do
 		{a, b, c} = :os.timestamp
 		a*1000000000000 + b*1000000 + c
 	end
-	defp generate(fullopts = %{}, lambda) when is_function(lambda,2) do
+	defp generate(fullopts = %{write_to_file: true}, lambda) when is_function(lambda,2) do
 		unixtime = now |> Integer.to_string
 		filename = "#{@output_dir}/#{unixtime}.txt"
 		File.touch!(filename)
@@ -58,60 +59,76 @@ defmodule RandomexDemo.Main do
 		:ok = :file.close(io)
 		File.write!(@index_file, "#{unixtime} : #{Jazz.encode!(fullopts)}\n", [:append])
 	end
+	defp generate(fullopts = %{write_to_file: false}, lambda) when is_function(lambda,2), do: lambda.(fullopts, nil)
 
 	#
 	#	functions for different input cases
 	#
 
-	defp generate_main(%{ranges: ranges, samples: samples, sets: sets}, io) do
+	defp generate_main(fullopts = %{ranges: ranges, samples: samples, sets: sets}, io) do
 		Enum.each(ranges, fn(this_range) ->
 			Enum.each(samples, fn(this_sample) ->
 				Enum.each(1..sets, fn(_) ->
 					Enum.each(1..this_sample, fn(_) ->
 						bin = Randomex.range(0, this_range) |> Integer.to_string
-						:ok = :file.write(io, "#{bin}\n")
-						:ok = IO.puts(bin)
+						write_data(bin<>"\n", fullopts, io)
 					end)
 				end)
 			end)
 		end)
 	end
 
-	defp generate_diehard(%{diehard: n}, io) when (is_integer(n) and (n > 0)) do
+	defp generate_diehard(fullopts = %{diehard: n, write_to_file: wtf, stdout: stdout}, io) when (is_integer(n) and (n > 0)) do
 		case div(n,@sample2write) do
-			0 -> Enum.each(1..n, fn(_) -> :ok = :file.write(io, <<Randomex.uniform(@max32bit_int)::32>>) end)
+			0 -> generate_diehard_rest(n, fullopts, io)
 			s ->
-				:ok = :file.write(io,	Enum.to_list(1..s)
-										|> Exutils.pmap(1, fn(_) -> Enum.reduce(1..@sample2write, <<>>, fn(_,acc) -> acc<><<Randomex.uniform(@max32bit_int)::32>> end) end)
-										|> Enum.join)
+				bin =	Enum.to_list(1..s)
+						|> Exutils.pmap(1, fn(_) ->
+							Enum.reduce(1..@sample2write, <<>>, fn(_,acc) ->
+								int = Randomex.uniform(@max32bit_int)
+								if stdout, do: (:ok = IO.puts(int))
+								acc<><<int::32>>
+							end)
+						end)
+						|> Enum.join
+				if wtf, do: (:ok = :file.write(io, bin))
 				case rem(n,@sample2write) do
 					0 -> :ok
-					n -> Enum.each(1..n, fn(_) -> :ok = :file.write(io, <<Randomex.uniform(@max32bit_int)::32>>) end)
+					n -> generate_diehard_rest(n, fullopts, io)
 				end
 		end
-		IO.puts("DONE")
+	end
+	defp generate_diehard_rest(n, %{write_to_file: wtf, stdout: stdout}, io) do
+		Enum.each(1..n, fn(_) ->
+			int = Randomex.uniform(@max32bit_int)
+			if stdout, do: (:ok = IO.puts(int))
+			if wtf, do: (:ok = :file.write(io, <<int::32>>))
+		end)
 	end
 
-	defp generate_cards(%{decks: decks, cards: cards}, io) do
+	defp generate_cards(fullopts = %{decks: decks, cards: cards}, io) do
 		this_deck = Excards.Deck.new(cards)
 		Enum.each(1..decks, fn(_) ->
 			bin =	Excards.Deck.shuffle(this_deck)
 					|> Stream.map(fn(%Excards{suit: suit, value: value}) -> Atom.to_string(suit)<>"_"<>Atom.to_string(value) end)
 					|> Enum.join(" ")
-			:ok = :file.write(io, bin<>"\n")
-			:ok = IO.puts(bin)
+			write_data(bin<>"\n", fullopts, io)
 		end)
 	end
 
-	defp generate_balls(%{ball_min: min, ball_max: max, ball_num: num, spin_num: spin_num}, io) do
+	defp generate_balls(fullopts = %{ball_min: min, ball_max: max, ball_num: num, spin_num: spin_num}, io) do
 		Enum.each(1..spin_num, fn(_) ->
 			bin =	Enum.to_list(min..max)
 					|> Randomex.shuffle
 					|> Enum.take(num)
 					|> Enum.join(" ")
-			:ok = :file.write(io, bin<>"\n")
-			:ok = IO.puts(bin)
+			write_data(bin<>"\n", fullopts, io)
 		end)
+	end
+
+	defp write_data(bin, %{write_to_file: wtf, stdout: stdout}, io) do
+		if wtf, do: (:ok = :file.write(io,bin))
+		if stdout, do: (:ok = IO.write(bin))
 	end
 
 end
